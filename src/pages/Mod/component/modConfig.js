e9c7b3f56a0f1e99e79b5618aa00f9e90e98ab75
Kairlec <sunfokairlec@gmail.com>
2023-09-10 13:51:02 +0800

    fix mod configuration title show wrong selector

--- src/pages/Mod/component/modConfig.js
+++ src/pages/Mod/component/modConfig.js
@@ -81,6 +81,13 @@ const OptionSelect = ({mod, root, setRoot, defaultValues, defaultValuesMap, setD
                         .map((item) =>
                                 // eslint-disable-next-line react/jsx-key
                             {
+                                // 例如2928810007,2334209327都是这样的,options只有一个,而且就只是默认值,并且该项的description没有内容
+                                if (item.options.length === 1 && item.options[0].data === item.default && !item.options[0].description) {
+                                    /*                           在DST中,如果label为空字符串,就直接是显示空白行,这里用||会导致label为空也显示name,为了跟DST保持一样使用了??
+                                                                                                                                     ↓                     */
+                                    return <Divider key={item.label}><span style={{fontSize: "14px", fontWeight: "600"}}>{item.label ?? item.name}</span></Divider>
+                                }
+                                // TODO 还不知道哪些mod是这样的作为标题的,我目前没有发现
                                 if (item.name === 'Title' || item.name === '') {
                                     if (item.label === '') {
                                         return ""
