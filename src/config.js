7ca10d827d14848dbd7e99151a0e70b0b7b07985
hu, jinbo <hujb@paratera.com>
2024-02-11 12:15:02 +0800

    修复模组 options length 为 undefined 情况

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const headerFlag1 = "版本: 1.2.8.2"
+export const headerFlag1 = "版本: 1.2.8.3"
 export const headerFlag2 = "禁止商用"
--- src/pages/Mod2/ModConfigOptions/OptionsSelect/index.js
+++ src/pages/Mod2/ModConfigOptions/OptionsSelect/index.js
@@ -71,7 +71,7 @@ const OptionSelect = ({mod, defaultConfigOptionsRef, modConfigOptionsRef}) => {
                     >
                         {({index, style })=>{
                             const item = configurationOptions[index]
-                            if (item.options.length === 1 && item.options[0].data === item.default && !item.options[0].description) {
+                            if (item?.options?.length === 1 && item?.options[0]?.data === item?.default && !item?.options[0]?.description) {
                                 // 在DST中,如果label为空字符串,就直接是显示空白行,这里用||会导致label为空也显示name,为了跟DST保持一样使用了??
                                 return <div style={style}>
                                     <Divider key={generateUUID()}>
